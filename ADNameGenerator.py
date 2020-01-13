8fd099ae4ed3613031400d238f325b34d539ba61
helo86 <mariusklimmek@gmail.com>
2020-01-13 11:27:24 +0100

    Update ADNameGenerator.py

--- ADNameGenerator.py
+++ ADNameGenerator.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 """
 AD Name Generator:
-rogordon
+Vname + NName and Vname + . + NName
+Nname + VName and Nname + . + VName
+
+Examples:
 rogordo
 rogord
 rgordon
 rgordo
 rgord
+ro.gordon
+ro.gordo
+r.gordon
+r.gordo
 """
 
 vname = "robert"
@@ -24,3 +31,15 @@ for y in range(nnameLen):
 for x in range(vnameLen):
     for y in range(nnameLen):
         print(vname[0:x+1]+nname[0:nnameLen-y])
+
+for x in range(nnameLen):
+    for y in range(vnameLen):
+        print(nname[0:x+1]+vname[0:nnameLen-y])
+
+for x in range(vnameLen):
+    for y in range(nnameLen):
+        print(vname[0:x+1]+"."+nname[0:nnameLen-y])
+
+for x in range(nnameLen):
+    for y in range(vnameLen):
+        print(nname[0:x+1]+"."+vname[0:nnameLen-y])
